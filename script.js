4867b5aab7fdf66fb375f243f6465b31af030fb6
Betsy Olaussen <betsyolaussen@icloud.com>
2019-03-31 16:31:18 -0500

    initial object data

---
+++ script.js
@@ -0,0 +1,15 @@
+new Vue ({
+    el: "ragnarokLlamaFacts",
+    data: {
+        name: "Ragnarok",
+        height: "3 feet tall",
+        color: "white",
+        currentResidence: "Ballard, WA",
+        born: "Peru",
+        age: "5 years old",
+        weight: "150 pounds",
+        favoriteColor: "ocean blue",
+        favoriteCoffee: "iced americano",
+        bestFriendsName: "Penny"
+    }
+});
