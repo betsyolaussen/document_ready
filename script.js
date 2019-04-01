1b8bc01406998a7609dbb6650776e32a3ad22447
Betsy Olaussen <betsyolaussen@icloud.com>
2019-03-31 19:25:48 -0500

    list of facts created

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 new Vue ({
-    el: "ragnarokLlamaFacts",
+    el: "#llamaFacts",
     data: {
-        name: "Ragnarok",
-        height: "3 feet tall",
-        color: "white",
-        currentResidence: "Ballard, WA",
-        born: "Peru",
-        age: "5 years old",
-        weight: "150 pounds",
-        favoriteColor: "ocean blue",
-        favoriteCoffee: "iced americano",
-        bestFriendsName: "Penny"
+        object: {
+            name: "Ragnarok",
+            height: "3 feet tall",
+            color: "white",
+            currentResidence: "Ballard, WA",
+            born: "Peru",
+            age: "5 years old",
+            weight: "150 pounds",
+            favoriteColor: "ocean blue",
+            favoriteCoffee: "iced americano",
+            bestFriendsName: "Penny"
+        }     
     }
 });
